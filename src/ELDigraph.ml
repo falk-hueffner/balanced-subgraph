743df8f8a65ff2944b8163c3c3cbc33f1709fe52
Falk Hueffner <hueffner@minet.uni-jena.de>
2006-12-14 17:45:22 +0000

    Add ELDigraph.

---
+++ src/ELDigraph.ml
@@ -0,0 +1,75 @@
+(* ulp -- solve the undirected labeling problem
+   Copyright (C) 2006  Falk Hüffner
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  *)
+
+type 'a t = ('a IntMap.t * 'a IntMap.t) IntMap.t (* preds, succs *);;
+
+let empty = IntMap.empty;;
+
+let has_vertex = IntMap.has_key;;
+
+let new_vertex g =
+  let i = if IntMap.is_empty g then 0 else (IntMap.max_key g) + 1 in
+    IntMap.add g i (IntMap.empty, IntMap.empty), i
+;;
+
+let succs g i = let _, succs = IntMap.get g i in succs;;
+let preds g i = let preds, _ = IntMap.get g i in preds;;
+
+let connect g i j label =
+  if not (has_vertex g i && has_vertex g j) then invalid_arg "Graph.connect: invalid vertex";
+  let preds_i, succs_i = IntMap.get g i in
+  let preds_j, succs_j = IntMap.get g i in
+  let succs_i = IntMap.add succs_i j label in
+  let preds_j = IntMap.add preds_j i label in
+  let g = IntMap.set g i (preds_i, succs_i) in
+  let g = IntMap.set g j (preds_j, succs_j) in
+    g
+;;
+
+let fold_vertices f g x =
+  IntMap.fold
+    (fun accu i (preds, succs) -> f accu i preds succs)
+    g
+    x
+;;
+let iter_vertices f g = fold_vertices (fun () i preds succs -> f i preds succs) g ();;
+
+let fold_edges f g accu =
+  fold_vertices
+    (fun accu i _ succs ->
+       IntMap.fold
+         (fun accu j label -> f accu i j label)
+         succs
+         accu)
+    g
+    accu
+;;
+
+let iter_edges f g = fold_edges (fun () i j l -> f i j l) g ();;
+
+let output channel output_label g =
+  Printf.fprintf channel "{\n";
+  (* List degree-0 vertices.  *)
+  iter_vertices (fun i preds succs ->
+                   if IntMap.is_empty preds && IntMap.is_empty succs
+                   then Printf.fprintf channel "%d\n" i) g;
+  iter_edges (fun i j l ->
+		Printf.fprintf channel "%d %d " i j;
+		output_label channel l;
+		Printf.fprintf channel "\n") g;
+  Printf.fprintf channel "}\n";
+;;
---
+++ src/ELDigraph.mli
@@ -0,0 +1,59 @@
+(* ulp -- solve the undirected labeling problem
+   Copyright (C) 2006  Falk Hüffner
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  *)
+
+(** Edge-labeled directed functional graphs. Vertices are represented
+    by nonnegative integers. No multiple edges are possible (but
+    self-loops are).  *)
+
+(** The type of a graph, parameterized by the edge label type.  *)
+type 'a t
+
+(** The empty graph.  *)
+val empty : 'a t
+
+(** [succs g i] returns a map that maps successors of [i] in [g] to
+    the corresponding edge label. Raises [Not_found] if [i] is not in [g].
+    O(log n) time.  *)
+val succs : 'a t -> int -> 'a IntMap.t
+
+(** [preds g i] returns a map that maps predecessors of [i] in [g] to
+    the corresponding edge label. Raises [Not_found] if [i] is not in [g].
+    O(log n) time.  *)
+val preds : 'a t -> int -> 'a IntMap.t
+
+(** [new_vertex g] returns [g', i], where [g'] is [g] with an
+    additional new vertex [i]. O(log n) time.  *)
+val new_vertex : 'a t -> 'a t * int
+
+(** [connect g v w l] returns [g] with an additional arc from [v] to
+    [w] labeled by [l].  Raises [Not_found] when [v] or [w] do not exist
+    in [g]. Raises [IntMap.Already_present] when there is already an arc
+    from [v] to [w]. O(log n) time.  *)
+val connect : 'a t -> int -> int -> 'a -> 'a t
+
+(** [fold_edges f g a] computes [(f iN jN lN ... (f i1 j1 lN a)...)],
+    where [(i1, j1) ... (iN, jN)] are the edges of [g], and [lN] is the
+    label of [(iN, jN)]. O(m) time.  *)
+val fold_edges : ('b -> int -> int -> 'a -> 'b) -> 'a t -> 'b -> 'b
+
+(** [iter_edges f g] calls [f u v l] for each edge [(u, v)] with label
+    [l] in [g]. O(m) time.  *)
+val iter_edges : (int -> int -> 'a -> unit) -> 'a t -> unit
+
+(** [output c output_label g] prints a debug representation of [g] to
+    channel [c]. O(m) time.  *)
+val output : out_channel -> (out_channel -> 'a -> unit) -> 'a t -> unit
