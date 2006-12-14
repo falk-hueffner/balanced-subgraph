441d00103c74db7f8a17c8a6bd2a18e117b3cb0c
Falk Hueffner <hueffner@minet.uni-jena.de>
2006-12-14 21:05:46 +0000

    New.

---
+++ src/Cut.ml
@@ -0,0 +1,33 @@
+(* ulp -- solve the undirected labeling problem
+   Copyright (C) 2006  Falk Hüffner
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program; if not, write to the Free Software Foundation, Inc.,
+   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  *)
+
+let vertex_cut_transform g =
+  let d = (Graph.max_vertex g) + 1 in
+  let entry i = i in
+  let outlet i = i + d in
+  let g' = Graph.fold_vertices
+    (fun g' i _ -> Digraph.add_vertex (Digraph.add_vertex g' i) (i + d))
+    g
+    Digraph.empty in
+  Graph.fold_edges
+    (fun g' i j ->
+       let g' = Digraph.connect g' (outlet i) (entry j) in
+       let g' = Digraph.connect g' (outlet j) (entry i) in
+	 g')
+    g
+    g'
+;;
