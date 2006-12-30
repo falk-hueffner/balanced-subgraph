715a05c5b5a007d7bba6aa647071909f0abac9bc
Falk Hueffner <hueffner@minet.uni-jena.de>
2006-12-30 18:38:21 +0000

    Cleanup gadget enumeration

--- src/ulp.ml
+++ src/ulp.ml
@@ -15,8 +15,8 @@
    with this program; if not, write to the Free Software Foundation, Inc.,
    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  *)
 
-module StringMap = Map.Make(struct type t = string    let compare = String.compare end);;
-module GadgetMap = Map.Make(struct type t = int array let compare = compare end);;
+module StringMap = Map.Make(struct type t = string   let compare = String.compare end);;
+module GadgetMap = Map.Make(struct type t = int list let compare = compare end);;
 
 type sign = Equal | Unequal
 type edge = {
@@ -136,7 +136,6 @@ let solve_brute_force g =
 ;;
 
 let reduce_cut g s c =
-  let g' = ELGraph.subgraph g (IntSet.union s c) in
 (*   Printf.eprintf "reduce_cut c = %a g = %a\n" IntSet.output s (fun c -> ELGraph.output c output_edge) g'; *)
   let merge g v1 v2 =
 (*     Printf.eprintf "merge %d %d\n" v1 v2; *)
@@ -249,9 +248,21 @@ let solve g =
 	components;
 ;;
 
+(* let gadgets = ref GadgetMap.empty;; *)
+
 let find_gadgets c_size s_size max_mult =
   let g = Util.fold_n ELGraph.add_vertex (c_size + s_size) ELGraph.empty in
-  ELGraph.output stdout output_edge g;
+(*   ELGraph.output stdout output_edge g;  *)
+  let fold_pairs f n a =
+    let rec loop a i j =
+      if i >= n then a
+      else if j >= i then loop a (i + 1) 0
+      else loop (f a i j) i (j + 1)
+    in
+      loop a 0 0 in
+  let n = ELGraph.num_vertices g in
+  let edges = fold_pairs (fun l i j -> (i, j) :: l) n [] in
+  let m = List.length edges in
   let l_min = -max_mult and l_max = max_mult in
   let l = Array.make m l_min in
   let bump () =
@@ -262,65 +273,14 @@ let find_gadgets c_size s_size max_mult =
       then begin l.(i) <- l_min; loop (i + 1) end
       else begin l.(i) <- l.(i) + 1; true end
     in
-      loop 0
+      loop 0 in
+  let c_set = Util.fold_n IntSet.add c_size IntSet.empty in
+  let s_set = Util.fold_n (fun s i -> IntSet.add s (i + c_size)) s_size IntSet.empty
   in
+(*     Printf.printf "c = %a s = %a\n" IntSet.output c_set IntSet.output s_set; *)
     l.(0) <- l_min - 1;
     while bump () do
-    done
-
-;;
-
-let () =
-  (*
-  let g, vertex_numbers, vertex_names = input_signed_graph stdin in
-  let m = ELGraph.fold_edges (fun m i j { equal = eq; unequal = ne } -> if i > j then m else m + eq + ne) g 0 in
-    ELGraph.output stdout output_edge g;
-    Printf.printf "m = %d\n" m;
-  *)
-  find_gadgets 3 1 2
-    
-    (***
-  let g, vertex_numbers, vertex_names = input_signed_graph stdin in
-    solve g;
-    exit 0;
-    ***)
-    (*
-    ELGraph.output stderr output_edge g;
-    let s = IntSet.of_list [3; 4] in
-    let c = IntSet.of_list [0; 1; 2] in
-      reduce_cut g s c
-    *)
-
-
-(*
-  
-  let g = ELGraph.empty in
-  let g = ELGraph.add_vertex g 0 in
-  let g = ELGraph.add_vertex g 1 in
-  let g = ELGraph.add_vertex g 2 in
-  let g = ELGraph.add_vertex g 3 in
-(*   let g = ELGraph.add_vertex g 4 in *)
-  let edges = [(0, 1); (1, 2); (0, 2)] in
-  let edges = [(0, 1); (1, 2); (0, 2); (0, 3); (1, 3); (2, 3)] in
-  let s = IntSet.empty in
-  let s = IntSet.singleton 3 in
-(*   let edges = [(0, 1); (1, 2); (0, 2); (0, 3); (1, 3); (2, 3); (0, 4); (1, 4); (2, 4); (3, 4)] in *)
-  let m = List.length edges in
-  let l_min = -5 and l_max = 5 in
-  let l = Array.make m l_min in
-  let bump () =
-    let rec loop i =
-      if i >= Array.length l
-      then false
-      else if l.(i) >= l_max
-      then begin l.(i) <- l_min; loop (i + 1) end
-      else begin l.(i) <- l.(i) + 1; true end
-    in
-      loop 0
-  in
-    l.(0) <- l_min - 1;
-    while bump () do
-(*       print_string "**l = "; Array.iter (fun i -> Printf.printf "%d " i) l; print_newline(); *)
+(*       Printf.printf "l = %a\n" (Util.output_array Util.output_int) l; *)
       let g, _ = List.fold_left
 	(fun (g, i) (v, w) ->
 	   if l.(i) > 0
@@ -331,41 +291,32 @@ let () =
 	(g, 0)
 	edges
       in
-(* 	ELGraph.output stdout output_edge g; *)
-	let r = reduce_cut g s (IntSet.of_list [0; 1; 2]) in
-(* 	let r = reduce_cut g (IntSet.of_list [3]) (IntSet.of_list [0; 1; 2]) in *)
- 	let m = List.fold_left min max_int r in
+(*  	ELGraph.output stdout output_edge g; *)
+	let r = reduce_cut g s_set c_set in
+(* 	  Util.output_list stdout Util.output_int r; *)
+(*  	let m = List.fold_left min max_int r in *)
 (*  	let r = List.map (fun i -> i - m) r in *)
 (* 	  Util.output_list stdout Util.output_int r; *)
 (* 	  ELGraph.output stdout output_edge g; *)
-	  List.iter (fun i -> Printf.printf "%d " i) r;
+ 	  List.iter (fun i -> Printf.printf "%d " i) r;
 	  print_newline ();
+	  (*
+	  if GadgetMap.has_key r !gadgets
+	  then 
+	  gadgets := GadgetMap.add r (Array.copy l) !gadgets;
+	  *)	
     done
-	
-(*     solve_brute_force g; *)
-    
-(*     ELGraph.output stderr output_edge g; *)
-(*     solve g; *)
-    (*
-  let components = Cut.biconnected_components (ELGraph.unlabeled g) in
-    List.iter
-      (fun component ->
-	 let g' = ELGraph.subgraph g component in
-	   ELGraph.output stdout output_edge g')
-      components;
-    *)
-    (*
-  let l = Cut.cut_corner (ELGraph.unlabeled g) in
-    List.iter
-      (fun (s, exits) ->
-	 if IntSet.size exits <= 2 then
-	   Printf.eprintf "exits = %a g = %a\n"
-	     IntSet.output exits
-	     (fun channel g -> ELGraph.output channel output_edge g) (ELGraph.subgraph g s))
-      l
-    *)
 
-*)
+;;
 
+let () =
+  let g, vertex_numbers, vertex_names = input_signed_graph stdin in
+
+  (* find_gadgets 4 1 1 *)
+  (* ELGraph.output stderr output_edge g; *)
+
+  solve g;
+  
+ 
   
 ;;
