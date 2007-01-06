00d3dc55561b3e28f34ba07e589905e46c4cf61e
Falk Hueffner <hueffner@minet.uni-jena.de>
2007-01-06 15:53:22 +0000

    Simplify.

--- src/Ulp.ml
+++ src/Ulp.ml
@@ -15,7 +15,7 @@
    with this program; if not, write to the Free Software Foundation, Inc.,
    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  *)
 
-module StringMap = Map.Make(struct type t = string let compare = String.compare end);;
+module StringMap = Map.Make(string);;
 
 type sign = Eq | Ne
 
