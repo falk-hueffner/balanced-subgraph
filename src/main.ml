67d0cf3ceecafebe3444b40b3ec61561f1c658d2
Falk Hueffner <hueffner@minet.uni-jena.de>
2007-01-10 16:49:03 +0000

    Cleanup.

--- src/main.ml
+++ src/main.ml
@@ -63,24 +63,25 @@ let find_lincomb v vs =
 	      loop v vs' max_cost in
   let v = normalize v in
   let rec trial v max_cost max_max_cost =
-(*     Printf.eprintf "trial max_cost = %d max_max_cost = %d\n" max_cost max_max_cost; *)
+(*     Printf.eprintf "trial max_cost = %d max_max_cost = %d\n%!" max_cost max_max_cost; *)
     if max_cost > max_max_cost
     then None
       else
 	match loop v vs max_cost with
 	    None -> trial v (max_cost + 1) max_max_cost
 	  | something -> something in
-  let rec shift v d =
-(*     Printf.eprintf "shift %d\n" d; *)
-    let v = Array.map ((+) d) v in
-    let max_max_cost = Array.fold_left (+) 0 v in
-      match trial v 0 max_max_cost with
-	  None -> shift v (d + 1)
-	| something -> something
+  let rec shift d =
+    if d >= 2 then None
+    else begin
+(*       Printf.eprintf "shift %d: %a\n%!" d (Util.output_array Util.output_int) v; *)
+      let v = Array.map ((+) d) v in
+      let max_max_cost = ((Array.fold_left (+) 0 v) + 1) / 2 in (* every vec. has at least 2 1s *)
+	match trial v 0 max_max_cost with
+	    None -> shift (d + 1)
+	  | Some (cost, ds, gadgets) -> Some (cost + d, ds, gadgets)
+    end
   in
-    match shift v 0 with
-	None -> assert false
-      | Some thing -> thing
+    shift 0
 ;;
 
 let fold_pairs f s1 s2 accu =
@@ -91,17 +92,18 @@ let fold_pairs f s1 s2 accu =
 let add_gadget gadgets g c_set cost =
   let colorings = Ulp.solve_all_colorings g c_set in
 (*   let costs = IntMap.map (fun _ coloring -> Ulp.coloring_cost g coloring) colorings in *)
+  let edges = ELGraph.fold_edges (fun r i j l -> (i, j, l) :: r) g [] in
   let costs = Array.init
     (IntMap.size colorings)
-    (fun i ->  Ulp.coloring_cost g (IntMap.get colorings i)) in
-  let do_replace =
-    if not (Hashtbl.mem gadgets costs) then true else
-      let oldcost, edges = Hashtbl.find gadgets costs in
-	oldcost > cost || (oldcost = cost && List.length edges > ELGraph.num_vertices g)
+    (fun i ->  Ulp.coloring_cost g (IntMap.get colorings i))
   in
-    if do_replace then
-      Hashtbl.replace
-	gadgets costs (cost, (ELGraph.fold_edges (fun r i j l -> (i, j, l) :: r) g []))
+(*     Printf.printf "%d %a\n%!" cost (Util.output_array Util.output_int) costs; *)
+    if (* true ||  *)
+      match find_lincomb costs gadgets with
+	  None -> ((* Printf.printf "fresh\n%!"; *) true)
+	| Some (cost', _, _) ->  ((* Printf.printf "old: %d new: %d\n%!" cost' cost; *) cost < cost')
+    then (cost, costs, edges) :: gadgets
+    else gadgets
 ;;
 
 let print_gadget costs cost edges =
@@ -123,10 +125,15 @@ let single_edge_gadgets gadgets c_size =
   let n = ELGraph.num_vertices g in
   let edges = fold_pairs (fun l i j -> if i < j then (i, j) :: l else l) c_set c_set [] in
   let m = List.length edges in
-    List.iter
-      (fun (i, j) ->
-	 add_gadget gadgets (ELGraph.connect g i j {Ulp.eq = 1; Ulp.ne = 0} ) c_set 0;
-	 add_gadget gadgets (ELGraph.connect g i j {Ulp.eq = 0; Ulp.ne = 1} ) c_set 0;) edges;
+    List.fold_left
+      (fun gadgets (i, j) ->
+	 let gadgets =
+	   add_gadget gadgets (ELGraph.connect g i j {Ulp.eq = 1; Ulp.ne = 0} ) c_set 0 in
+	 let gadgets =
+	   add_gadget gadgets (ELGraph.connect g i j {Ulp.eq = 0; Ulp.ne = 1} ) c_set 0
+	 in
+	   gadgets)
+      gadgets edges
 ;;
 
 let extra_vertices_gadgets gadgets c_size s_size =
@@ -151,69 +158,85 @@ let extra_vertices_gadgets gadgets c_size s_size =
       loop 0
   in
     l.(0) <- l_min - 1;
-    while bump () do
-      let g, _ = List.fold_left
-	(fun (g, i) (v, w) ->
-	   if l.(i) > 0
-	   then ELGraph.connect g v w { Ulp.eq = l.(i); Ulp.ne = 0 }, i + 1
-	   else if l.(i) < 0
-	   then ELGraph.connect g v w { Ulp.eq = 0; Ulp.ne = -l.(i) }, i + 1
-	   else g, i + 1)
-	(g, 0)
-	edges in
-      let s_c_edges =
-	fold_pairs
-	  (fun s_c_edges i j -> s_c_edges + if ELGraph.is_connected g i j then 1 else 0)
-	  s_set c_set 0 in
-	if s_c_edges >= 3 && ELGraph.is_connected_graph (ELGraph.subgraph g s_set)
-	then
-	  add_gadget gadgets g c_set s_size;
-    done
+    let rec loop gadgets =
+      if not (bump ()) then gadgets
+      else
+	let g, _ = List.fold_left
+	  (fun (g, i) (v, w) ->
+	     if l.(i) > 0
+	     then ELGraph.connect g v w { Ulp.eq = l.(i); Ulp.ne = 0 }, i + 1
+	     else if l.(i) < 0
+	     then ELGraph.connect g v w { Ulp.eq = 0; Ulp.ne = -l.(i) }, i + 1
+	     else g, i + 1)
+	  (g, 0)
+	  edges in
+	let s_c_edges =
+	  fold_pairs
+	    (fun s_c_edges i j -> s_c_edges + if ELGraph.is_connected g i j then 1 else 0)
+	    s_set c_set 0 in
+	let gadgets =
+	  if s_c_edges >= 3 && ELGraph.is_connected_graph (ELGraph.subgraph g s_set)
+	  then add_gadget gadgets g c_set s_size
+	  else gadgets
+	in
+	  loop gadgets
+    in
+      loop gadgets
 ;;
 
-
 let () =
-  let gadgets = Hashtbl.create 100 in
-    single_edge_gadgets gadgets 4;
-    extra_vertices_gadgets gadgets 4 1;
-    extra_vertices_gadgets gadgets 4 2;
-    Hashtbl.iter
-      (fun costs (cost, edges) -> print_gadget costs cost edges; print_newline ())
-      gadgets;
-    let gadget_list =
-      Hashtbl.fold (fun costs (cost, edges) l -> (cost, costs, edges) :: l) gadgets [] in
+  let gadgets = [] in
+  let gadgets = single_edge_gadgets gadgets 3 in
+  let gadgets = extra_vertices_gadgets gadgets 3 1 in
+    (*
+    prerr_string "--------------\n";
+  let gadgets = extra_vertices_gadgets gadgets 5 2 in
+  let gadgets = extra_vertices_gadgets gadgets 5 3 in
+    *)
+    List.iter
+      (fun (cost, costs, edges) -> print_gadget costs cost edges; print_newline ())
+      (List.sort compare gadgets);
+    (*
     let vv = [
-      [| 3; 3; 2; 4; 4; 3; 3; 4 |];
-      [| 5; 7; 5; 7; 7; 6; 7; 6 |];
-      [| 5; 5; 7; 7; 7; 7; 7; 7 |];
-      [| 3; 5; 5; 5; 4; 5; 6; 4 |];
-      [| 2; 4; 4; 5; 3; 5; 5; 4 |];
-      [| 5; 5; 4; 4; 4; 5; 5; 5 |];
-      [| 4; 7; 6; 7; 8; 9; 8; 7 |];
-      [| 6; 5; 5; 6; 6; 5; 5; 6 |];
-      [| 6; 5; 5; 4; 5; 4; 4; 3 |];
-      [| 5; 4; 5; 4; 4; 5; 4; 5 |];
-      [| 7; 7; 5; 7; 9; 7; 7; 7 |];
-      [| 2; 3; 3; 2; 3; 3; 4; 2 |];
-      [| 8; 7; 8; 7; 7; 8; 7; 8 |];
-      [| 4; 4; 4; 4; 4; 4; 4; 4 |];
-      [| 6; 6; 5; 5; 7; 5; 7; 6 |];
-      [| 1; 2; 4; 1; 2; 3; 3; 0 |]
+(*       [| 3; 3; 2; 4; 4; 3; 3; 4 |]; *)
+(*       [| 5; 7; 5; 7; 7; 6; 7; 6 |]; *)
+(*       [| 5; 5; 7; 7; 7; 7; 7; 7 |]; *)
+(*       [| 3; 5; 5; 5; 4; 5; 6; 4 |]; *)
+(*       [| 2; 4; 4; 5; 3; 5; 5; 4 |]; *)
+(*       [| 5; 5; 4; 4; 4; 5; 5; 5 |]; *)
+(*       [| 4; 7; 6; 7; 8; 9; 8; 7 |]; *)
+(*       [| 6; 5; 5; 6; 6; 5; 5; 6 |]; *)
+(*       [| 6; 5; 5; 4; 5; 4; 4; 3 |]; *)
+(*       [| 5; 4; 5; 4; 4; 5; 4; 5 |]; *)
+(*       [| 7; 7; 5; 7; 9; 7; 7; 7 |]; *)
+(*       [| 2; 3; 3; 2; 3; 3; 4; 2 |]; *)
+(*       [| 8; 7; 8; 7; 7; 8; 7; 8 |]; *)
+(*       [| 4; 4; 4; 4; 4; 4; 4; 4 |]; *)
+(*       [| 6; 6; 5; 5; 7; 5; 7; 6 |]; *)
+(*       [| 1; 2; 4; 1; 2; 3; 3; 0 |]; *)
+      [| 4; 6; 4; 4; 5; 7; 6; 5; 4; 7; 6; 6; 3; 5; 5; 5 |]; (* 2 enough!  *)
+      [| 5; 5; 9; 6; 4; 7; 7; 6; 6; 6; 8; 5; 5; 8; 6; 6 |]; (* 2 not enough, apparently... *)
+      [| 9; 9; 8; 8; 6; 6; 7; 7; 8; 8; 7; 7; 7; 7; 8; 8 |];
+      [| 8; 7; 7; 8; 8; 7; 7; 8; 8; 7; 7; 8; 8; 7; 7; 8 |];
+      [| 9; 9; 8; 8; 9; 9; 8; 8; 8; 8; 9; 9; 8; 8; 9; 9 |];
+      [| 9; 8; 8; 6; 9; 9; 9; 6; 9; 8; 9; 6; 9; 9; 11; 8 |];      
     ]
     in
     List.iter
       (fun v ->
 	 Util.output_array Util.output_int stdout v; print_newline ();
-	 let cost, costvecs, gadgets = find_lincomb v gadget_list in
-	   Printf.printf "costs: %d\n" cost;
-	   List.iter
-	     (fun costvec ->
-		Printf.printf "%a\n" (Util.output_array Util.output_int) costvec)
-	     costvecs;
-	   print_newline ();
+	 match find_lincomb v gadgets with
+	     None -> ()
+	   | Some (cost, costvecs, gadgets) ->
+	       Printf.printf "costs: %d\n" cost;
+	       List.iter
+		 (fun costvec ->
+		    Printf.printf "%a\n" (Util.output_array Util.output_int) costvec)
+		 costvecs;
+	       print_newline ();
       ) vv;
+    *)
     exit 0;
-    
 ;;
 
 (*
