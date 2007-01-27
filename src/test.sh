cf914d2ff0896dcb0326c8f379a51c36b712a718
Falk Hueffner <hueffner@minet.uni-jena.de>
2007-01-27 19:17:52 +0000

    use tests from list

--- src/test.sh
+++ src/test.sh
@@ -2,7 +2,16 @@
 
 ulimit -t 60
 
-for i in test/*graph; do
-  printf "%20s " ${i#test/}
-  ./ulp -s < $i || echo
+error=0
+
+cat test/tests | \
+while read g k; do
+  printf "%s\t" $g
+  kk=$(./ulp -s < test/$g.graph | tee /dev/stderr | awk '{print $3}')
+  if [ $k -ne $kk ]; then
+    printf "FAILED %s: correct %d result %d\n" $g $k $kk
+    error=1
+  fi
 done
+
+exit $error
