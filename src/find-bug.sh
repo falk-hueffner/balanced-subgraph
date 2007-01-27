5f24dc3b0f280a185e99a2a26993c9f15e32cde7
Falk Hueffner <hueffner@minet.uni-jena.de>
2007-01-27 20:03:58 +0000

    tweak

--- src/find-bug.sh
+++ src/find-bug.sh
@@ -1,12 +1,12 @@
 #! /bin/zsh
 
-for n in {2..100}; do
-  for m in {1..$((n * (n-1) / 2))}; do
+for n in {6..100}; do
+  for m in {$((n/2))..$((n * (n-1) / 2))}; do
     for k in {0..$m}; do
       printf "n = %d m = %d k = %d\n" n m k
-      for i in {1..100}; do
+      for i in {1..10}; do
         ./gen-ulp $n $m $k > test.graph
-	k1=$(./ulp.run -s < test.graph | awk '{print $3}')
+	k1=$(./ulp -s < test.graph | awk '{print $3}')
 	k2=$(./ulp-lp -e -s < test.graph | awk '{print $3}')
 	[ x"$k1" != x"$k2" ] && exit
       done
