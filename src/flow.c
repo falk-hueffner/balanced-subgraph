6d673e025dda59b6f8fed21de5bd6fac609cba7f
Falk Hüffner <falk@debian.org>
2026-09-19 18:56:40 +0200

    Convert everything to UTF-8.

--- src/flow.c
+++ src/flow.c
@@ -1,5 +1,5 @@
 /* bsg -- solve the balanced subgraph problem
-   Copyright (C) 2006  Falk Hüffner
+   Copyright (C) 2006  Falk HÃ¼ffner
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
